2ced6f9ebc8148d0f9250c69baff3314e48d0f32
Beomgu <jb98967@gmail.com>
2025-01-18 09:39:57 -0500

    [Feat] Add sample page for ejs and install required packages

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 
 const PORT = process.env.HTTP_PORT || 8080;
 
+app.use(express.json());
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// ejs
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// public folder
+app.use(express.static(path.join(path.dirname(require.main.filename), 'public')));
+
 app.get('/', (req, res, next) => {
-  res.send('Hello');
+  res.render('index', {
+    message: 'This is the test',
+  });
 });
 
 app.listen(PORT, () => {

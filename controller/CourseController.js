72fd86520eaddbc859043c2f38f7a058510f122d
Harrison Nguyen <hnguyen169@myseneca.ca>
2025-01-18 15:51:24 -0500

    completed createCourse, getCourseList, getCourse, updateCourse, deleteCourse functions

--- controller/CourseController.js
+++ controller/CourseController.js
@@ -2,27 +2,80 @@ const Course = require('../model/Course');
 const Utils = require('../util/Utils');
 const CourseService = require('../service/CourseService');
 
-exports.createCourse = (req, res, next) => {
-  // TODO: call function to create course and handle the error if it is
-  //       After, send a proper response
+exports.createCourse = async (req, res, next) => {
+    try {
+        const {course_name, description} = req.body;
+
+        if (!course_name || !description) {
+            return Utils.response(res, 400, 'Please provide all the required fields');
+        }
+
+        const course = await CourseService.createCourse({course_name, description});
+
+        return Utils.response(res, 201, 'Course created successfully', course);
+    }
+    catch (err) {
+        return Utils.response(res, 500, err.message);
+    }
 };
 
-exports.getCourseList = (req, res, next) => {
-  // TODO: call function to get course list and handle the error if it is
-  //       After, send a proper response
+exports.getCourseList = async (req, res, next) => {
+    try {
+    const courses = await CourseService.getCourseList();
+
+    return Utils.response(res, 200, 'Courses fetched successfully', courses);
+    }
+    catch (err) {
+        return Utils.response(res, 500, err.message);
+    }
 };
 
-exports.getCourse = (req, res, next) => {
-  // TODO: call function to get course and handle the error if it is
-  //       After, send a proper response
+exports.getCourse = async (req, res, next) => {
+    try {
+        const courseId = req.params.courses;
+        const course = await CourseService.getCourse(courseId);
+    
+        if (!course) {
+            return Utils.response(res, 404, 'Course not found');
+        }
+
+        return Utils.response(res, 200, 'Course fetched successfully', course);
+    }
+    catch (err) {
+        return Utils.response(res, 500, err.message);
+    }
 };
 
 exports.updateCourse = (req, res, next) => {
-  // TODO: call function to update course and handle the error if it is
-  //       After, send a proper response
+    try {
+        const courseId = req.params.courses;
+        const {course_name, description} = req.body;
+        const updatedCourse = await CourseService.updateCourse(courseId, {course_name, description});
+    
+        if (!updatedCourse) {
+            return Utils.response(res, 404, 'Course not found');
+        }
+
+        return Utils.response(res, 200, 'Course updated successfully', updatedCourse);
+    }
+    catch (err) {
+        return Utils.response(res, 500, err.message);
+    }
 };
 
 exports.deleteCourse = (req, res, next) => {
-  // TODO: call function to delete course and handle the error if it is
-  //       After, send a proper response
+    try {
+        const courseId = req.params.courses;
+        const {course_name, description} = req.body;
+        const updatedCourse = await CourseService.deleteCourse(courseId, {course_name, description});
+    
+        if (!updatedCourse) {
+            return Utils.response(res, 404, 'Course not found');
+        }
+
+        return Utils.response(res, 200, 'Course deleted successfully', updatedCourse);
+    }
+    catch (err) {
+        return Utils.response(res, 500, err.message);
+    }
 };

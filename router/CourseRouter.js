a9f2bdb48e52efacf7d38707fb9c6e76a25f061e
Beomgu <jb98967@gmail.com>
2025-01-18 11:23:52 -0500

    [Fix] Fix typo

--- router/CourseRouter.js
+++ router/CourseRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const courseController = require('../controller/courseController');
+const courseController = require('../controller/CourseController');
 
 const router = express.Router();
 
